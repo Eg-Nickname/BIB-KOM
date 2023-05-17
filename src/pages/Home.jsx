f33986228ca87ee804de5ef58d95e35ffc462a00
Alt+F4 <mail@example.com>
2023-05-17 19:44:03 +0200

    Home page grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,30 +7,38 @@ function Home() {
     <div>
       <Navbar />
       <div className="pageWidth">
-        <div>
-          <p>
-            Jesteśmy osobami z pasją. Wykonujemy swoją pracę z zamiłowania i
-            dużym zaangażowaniem. Profesjonalna wiedza, którą zdobywaliśmy przez
-            lata pozwala nam na rozwiązywanie wszelkich problemów z dziedziny
-            informatyki i teleinformatyki.
-          </p>
-          <img src={placeholder}></img>
-        </div>
-        <div>
-          <img src={placeholder}></img>
-          <p>
-            Naszą mocną stroną jest indywidualne podejście do klientów.
-            Dokonujemy wycen oraz doboru sprzętu z uwzględnieniem ich potrzeb i
-            wymagań. Większość diagnoz uszkodzonego sprzętu dokonujemy na
-            poczekaniu i bezpłatnie.
-          </p>
-        </div>
-        <div>
-          <p>
-            Oferujemy dojazd do klienta w dogodnym czasie mając na względzie
-            seniorów oraz osoby mające ograniczoną mobilność.
-          </p>
-          <img src={placeholder}></img>
+        <div className="grid-wrapper">
+          <div className="grid-text">
+              Jesteśmy osobami z pasją. Wykonujemy swoją pracę z zamiłowania i
+              dużym zaangażowaniem. Profesjonalna wiedza, którą zdobywaliśmy przez
+              lata pozwala nam na rozwiązywanie wszelkich problemów z dziedziny
+              informatyki i teleinformatyki.
+          </div>
+          <div>
+            <img className="grid-image" src={placeholder}></img>
+          </div>
+
+          <div className="spacer"></div>
+
+          <div>
+            <img className="grid-image" src={placeholder}></img>
+          </div>
+          <div className="grid-text">
+              Naszą mocną stroną jest indywidualne podejście do klientów.
+              Dokonujemy wycen oraz doboru sprzętu z uwzględnieniem ich potrzeb i
+              wymagań. Większość diagnoz uszkodzonego sprzętu dokonujemy na
+              poczekaniu i bezpłatnie.
+          </div>
+
+          <div className="spacer"></div>
+
+          <div className="grid-text">
+              Oferujemy dojazd do klienta w dogodnym czasie mając na względzie
+              seniorów oraz osoby mające ograniczoną mobilność.
+          </div>
+          <div>
+            <img className="grid-image" src={placeholder}></img>
+          </div>
         </div>
       </div>
       <Footer />
