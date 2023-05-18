460abeffdd058a7e552ee3ae9db36719a24fdd02
Dimoon2134 <jacobfrye@interia.pl>
2023-05-18 10:17:33 +0200

    Main Page styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,13 @@ function Home() {
       <div className="pageWidth">
         <div className="grid-wrapper">
           <div className="grid-text">
+            <b>
               Jesteśmy osobami z pasją. Wykonujemy swoją pracę z zamiłowania i
-              dużym zaangażowaniem. Profesjonalna wiedza, którą zdobywaliśmy przez
-              lata pozwala nam na rozwiązywanie wszelkich problemów z dziedziny
-              informatyki i teleinformatyki.
+              dużym zaangażowaniem.
+            </b>
+            &nbsp; Profesjonalna wiedza, którą zdobywaliśmy przez lata pozwala
+            nam na rozwiązywanie wszelkich problemów z dziedziny informatyki i
+            teleinformatyki.
           </div>
           <div>
             <img className="grid-image" src={placeholder}></img>
@@ -20,21 +23,21 @@ function Home() {
 
           <div className="spacer"></div>
 
-          <div>
+          <div className="smth">
             <img className="grid-image" src={placeholder}></img>
           </div>
           <div className="grid-text">
-              Naszą mocną stroną jest indywidualne podejście do klientów.
-              Dokonujemy wycen oraz doboru sprzętu z uwzględnieniem ich potrzeb i
-              wymagań. Większość diagnoz uszkodzonego sprzętu dokonujemy na
-              poczekaniu i bezpłatnie.
+            Naszą mocną stroną jest indywidualne podejście do klientów.
+            Dokonujemy wycen oraz doboru sprzętu z uwzględnieniem ich potrzeb i
+            wymagań. Większość diagnoz uszkodzonego sprzętu dokonujemy na
+            poczekaniu i bezpłatnie.
           </div>
 
           <div className="spacer"></div>
 
           <div className="grid-text">
-              Oferujemy dojazd do klienta w dogodnym czasie mając na względzie
-              seniorów oraz osoby mające ograniczoną mobilność.
+            <b>Oferujemy dojazd do klienta</b> w dogodnym czasie mając na
+            względzie seniorów oraz osoby mające ograniczoną mobilność.
           </div>
           <div>
             <img className="grid-image" src={placeholder}></img>
