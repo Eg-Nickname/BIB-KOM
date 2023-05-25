fa45736a7db62af30984991ff18533f697643a2e
Alt+F4 <mail@example.com>
2023-05-25 10:47:06 +0200

    photoslider animation trying

--- src/components/photoSlider.jsx
+++ src/components/photoSlider.jsx
@@ -4,11 +4,15 @@ function PhotoSlider(props) {
   const { images } = props;
   const Interval = 4000;
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [prevIndex, setPrevIndex] = useState(images.length - 1);
   const dots = images.map((n, index) => (
     <li key={index}>
       <button
         className={currentIndex === index ? "dot active_dot" : "dot"}
-        onClick={() => setCurrentIndex(index)}
+        onClick={() => {
+          setPrevIndex(currentIndex)
+          setCurrentIndex(index)
+        }}
       ></button>
     </li>
   ));
@@ -20,21 +24,34 @@ function PhotoSlider(props) {
       clearInterval(interval);
     };
   });
-  // console.log(dots);
   const setPhoto = (n) => {
     if (currentIndex + n < images.length) {
+      setPrevIndex(currentIndex)
       setCurrentIndex((prevIndex) => prevIndex + n);
     } else {
+      setPrevIndex(currentIndex)
       setCurrentIndex(0);
     }
   };
   return (
     <div className="Photoslider">
-      <img className="Photosliderimages"
-        src={`src/assets/${images[currentIndex].src}`}
-        alt={images[currentIndex].text}
-      />
-      <div className="Photoslidertext">{images[currentIndex].text}</div>
+      <div className="PhotosWrapper">
+        <div className="prevPhoto">
+            <img className="Photosliderimages"
+              src={`src/assets/${images[prevIndex].src}`}
+              alt={images[prevIndex].text}
+            />
+            <div className="Photoslidertext">{images[prevIndex].text}</div>
+          </div>
+
+          <div className="currentPhoto">
+            <img className="Photosliderimages"
+              src={`src/assets/${images[currentIndex].src}`}
+              alt={images[currentIndex].text}
+            />
+        </div>
+        <div className="Photoslidertext">{images[currentIndex].text}</div>
+      </div>
       <ul className="slider_back">{dots}</ul>
     </div>
   );
