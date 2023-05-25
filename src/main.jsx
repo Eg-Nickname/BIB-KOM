c65722d8977240cc01af5e22d7ace2b6d22afb0c
Dimoon2134 <jacobfrye@interia.pl>
2023-05-25 10:20:23 +0200

    News

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home.jsx";
+import News from "./pages/News.jsx";
 import Offers from "./pages/Offers.jsx";
 import Contact from "./pages/Contact.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -13,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<Home />} />
         <Route path="/kontakt" element={<Contact />} />
         <Route path="/oferty" element={<Offers />} />
-        <Route path="/aktualności" element={<Home />} />
+        <Route path="/aktualności" element={<News />} />
         <Route path="/panel" element={<Home />} />
         <Route path="/*" element={<Home />} />
       </Routes>
---
+++ src/pages/list.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: "title1",
+    date: "20.0.2020",
+    text: "To jest tekst",
+  },
+  {
+    title: "costam",
+    date: "20.0.2021",
+    text: "To jest tekst2",
+  },
+];
