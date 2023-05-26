88d6603870a2a49eabc4f0a31e06544a1aa1a801
Dimoon2134 <jacobfrye@interia.pl>
2023-05-26 10:25:52 +0200

    Server.js

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,10 +1,13 @@
 function Post(props) {
-  const { title, date, text } = props.data;
+  const { title, date, text, src } = props.data;
+  const path = "/src/assets/postImages/";
+  const source = `${path}${src}`;
   return (
     <div className="post">
       <h1>{title}</h1>
       <h1>{date}</h1>
       <p>{text}</p>
+      <img src={source}></img>
     </div>
   );
 }
--- src/pages/list.js
+++ src/pages/list.js
@@ -3,10 +3,12 @@ export default [
     title: "title1",
     date: "20.0.2020",
     text: "To jest tekst",
+    src: "1.png",
   },
   {
     title: "costam",
     date: "20.0.2021",
     text: "To jest tekst2",
+    src: "2.png",
   },
 ];
